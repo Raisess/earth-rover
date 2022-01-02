9b21282d9a6f1501dce5fb7f842edd7397a64497
Raisess <danilosantana456789@gmail.com>
2022-01-02 18:45:27 -0300

    feat: env util

---
+++ src/utils/Env.lua
@@ -0,0 +1,35 @@
+local Str = require("Str");
+
+local _Env = {
+  data = {},
+};
+
+function _Env:set(property, value)
+  self.data[property] = value;
+end
+
+function _Env:get(property)
+  return self.data[property];
+end
+
+Env = {};
+
+function Env.parse()
+  local f = file.open(".env", "r");
+
+  while (true) do
+    local line = f:readline();
+
+    if (line == nil) then
+      f:close();
+      break;
+    end
+
+    local key_value = Str.split(line, "=");
+    _Env:set(key_value[1], key_value[2]);
+  end
+
+  return _Env;
+end
+
+return Env;
