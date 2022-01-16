ff65833382ded7f61a8ad315006388e4d367e6fd
Raisess <danilosantana456789@gmail.com>
2022-01-16 16:21:30 -0300

    feat: add a to_string function to str utils

--- src/utils/Str.lua
+++ src/utils/Str.lua
@@ -8,4 +8,14 @@ function Str.split(str, target)
   return tmp;
 end
 
+function Str.to_string(value)
+  if type(value) == "boolean" then
+    return value and "true" or "false";
+  elseif type(value) == "nil" then
+    return "nil";
+  end
+
+  return tostring(value);
+end
+
 return Str;
